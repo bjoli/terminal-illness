3e044cc75bc92cd3f9019f30b58197c2dc89d95c
Linus Björnstam <linus.github@fastmail.se>
2016-03-19 12:52:39 +0100

    Made it a proper scheme program so that I don't have to have use racket (and thus have X installed on my rpi2) tested in racket, guile and chicken

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # terminal-illness
 
-A simple racket library to write colourful text in the linux terminal. ISC-licensed.
+A simple scheme/racket library to write colourful text in the linux terminal. ISC-licensed.
 
 ## Warning
-I spent more time writing this document than I did writing the library. If you are looking to write anything serious, or if you are bothered by my proper spelling of colour, please head over to the much more ambitious project [terminal-color (SIC) by hopkinsr](https://github.com/hopkinsr/terminal-color), which not only has fancy documentation, but is also cross-platform.
+I spent more time writing this document than I did writing the library. If you are looking to write anything serious, or if you are bothered by my proper spelling of colour, please head over to the much more ambitious (racket-only) project [terminal-color (SIC) by hopkinsr](https://github.com/hopkinsr/terminal-color), which not only has fancy documentation, but is also cross-platform.
 
 This library was not planned in any way, and is not tested. It works for me. I use it to write colourful messages to a small LCD screen attached to my Raspberry Pi.
 
@@ -19,17 +19,13 @@ For the colours black, green, red, yellow, blue, magenta, cyan and white I provi
 (bg-_colour_ \[str\]) - Same as above, but changes the background colour.
 
 ### Bold
-(bold str) - prints str bold. Because of how bold works, there is no way to un-bold without resetting all other formatting.
-
-(start-bold) - returns the string to start writing bold. You have to display it for it to work.
+(bold \[str\]) - Returns a bold str. Because of how bold works, there is no way to un-bold without resetting all other formatting. Without str it just returns a start-bold escape char. 
 
 (stop-bold) -  returns reset formatting string. You have to display it for it to work. 
 
-(display-bold str)  and (displayln-bold str). Does what you think. 
-
 ### Underline
 
-(underline str) (start-underline) (stop-underline) (display-underline str) (displayln-underline str). Works as bold, but does not reset the formatting.
+(underline str) (stop-underline). Works as bold, but does not reset the formatting.
 
 ### Convenience functions
 
@@ -49,8 +45,8 @@ For the colours black, green, red, yellow, blue, magenta, cyan and white I provi
 
 I haven't gotten any questions at all actually, so I'm just making them up.
 
-**Why are you using Racket? I can't install it on my RPi without X! It's bloat!**  
-I wanted to use Chicken Scheme, but I was too stupid to grok their macro system in 10 minutes.
+**Why are you using Racket? I can't install it on my RPi without X! It's bloat!** 
+This was an old issue. Now it runs just fine with both guile and CHICKEN
 
 **Why didn't you just use terminal-color?**  
 Didn't know it existed, and when I found it I much preferred my way of doing/spelling it.
--- terminal-illness.rkt
+++ terminal-illness.rkt
@@ -1,76 +1,72 @@
 #lang racket/base
 (provide (all-defined-out))
+; to use with any r5rs implementation just comment the lines above and use as-is
+; To use with whateve r6rs or r7rs system you prefer, consult their documentation
 
-
-; Convenience functions so that you don't have to string-append (green "OK?") (space) (red "NO!")
+;Convenience functions so that you don't have to string-append (green "OK?") (space) (red "NO!")
 (define (space) " ")
 (define (blot . args)
-  (for ([s args])
-    (display s)))
+  (map display args) (values))
 
 (define (blotln . args)
-  (for ([s args])
-    (display s))
-  (newline))
+  (map display args)
+  (newline) (values))
 
 (define (blotlns . args)
-  (for ([s args])
-    (displayln s)))
-
+  (map (lambda (s) (display s) (newline)) args) (values))
 
 ; define for use within the macros
-(define default-style (make-parameter "\e[0m"))
+(define default-style (make-parameter "\x1b[0m"))
 
-(require (for-syntax racket/base))
-(define-syntax (define-style stx)
-  (syntax-case stx ()
-    [(_ name definition)
-     #'(define (name (str ""))
-         (cond
-           [(string=? str "") (default-style definition) definition]
-           [else (string-append definition str (default-style))]))]))
+(define-syntax define-style
+  (syntax-rules ()
+    ((_ name definition)
+     (define (name . args)
+       (cond
+         [(= (length args) 0) (default-style definition) definition]
+         [else (string-append definition (car args) (default-style))])))))
 
 ;Forground colours
-(define-style black "\e[30m")
-(define-style red "\e[31m")
-(define-style green "\e[32m")
-(define-style yellow "\e[33m")
-(define-style blue "\e[34m")
-(define-style magenta "\e[35m")
-(define-style cyan "\e[36m")
-(define-style white "\e[37m")
+(define-style black   "\x1b[30m")
+(define-style red     "\x1b[31m")
+(define-style green   "\x1b[32m")
+(define-style yellow  "\x1b[33m")
+(define-style blue    "\x1b[34m")
+(define-style magenta "\x1b[35m")
+(define-style cyan    "\x1b[36m")
+(define-style white   "\x1b[37m")
 
 ;Background colours
-(define-style bg-black "\e[40m")
-(define-style bg-red "\e[41m")
-(define-style bg-green "\e[42m")
-(define-style bg-yellow "\e[43m")
-(define-style bg-blue "\e[44m")
-(define-style bg-magenta "\e[45m")
-(define-style bg-cyan "\e[46m")
-(define-style bg-white "\e[47m")
+(define-style bg-black   "\x1b[40m")
+(define-style bg-red     "\x1b[41m")
+(define-style bg-green   "\x1b[42m")
+(define-style bg-yellow  "\x1b[43m")
+(define-style bg-blue    "\x1b[44m")
+(define-style bg-magenta "\x1b[45m")
+(define-style bg-cyan    "\x1b[46m")
+(define-style bg-white   "\x1b[47m")
 
 
 
 ;Text styles. These behave differently than the ones above
 
 ; Bold. There is no way to un-bold without resetting all other formatting
-(define (bold str) (string-append "\e[1m" str "\e[0m"))
-(define (start-bold) "\e[1m")
-(define (stop-bold) "\e[0m")
-(define (display-bold str) (display (bold str)))
-(define (displayln-bold str) (display-bold str) (newline))
+(define (bold . args)
+  (cond
+    [(= (length args) 0) "\x1b[1m"]
+    [else (string-append "\x1b[1m" (car args) "\x1b[0m")]))
+(define (stop-bold) "\x1b[0m")
+; Boring underline stuff.
+(define (underline . args) 
+  (cond
+    [(= (length args) 0) "\x1b[4m"]
+    [else (string-append "\x1b[4m" (car args) "\x1b[24m")]))
 
-; Boring underline stuff. 
-(define (underline str) (string-append "\e[4m" str "\e24m"))
-(define (display-underline str) (display (underline str)))
-(define (displayln-underline str) (display (underline str))(newline))
-(define (start-underline) "\e[4m")
-(define (stop-underline)  "\e[24m")
+(define (stop-underline)  "\x1b[24m")
 
 ; Clear screen
-(define (clear) "\e[3;J\e[H\e[2J")
+(define (clear) "\x1b[3;J\x1b[H\x1b[2J")
 (define (display-clear) (display (clear)))
 
-(define (reset) "\e[0m")
+(define (reset) "\x1b[0m")
 (define (display-reset) (display (reset)))
